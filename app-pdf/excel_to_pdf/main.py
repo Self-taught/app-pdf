cf2b8e7ca435c54329021ad0cdf0a0313fe13d13
Self-taught <63093667+Self-taught@users.noreply.github.com>
2023-03-09 09:50:58 +0530

    Basic code

---
+++ app-pdf/excel_to_pdf/main.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import glob
+from fpdf import FPDF
+from pathlib import Path
+
+filepaths = glob.glob("invoices/*.xlsx")
+
+for filepath in filepaths:
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    filename = Path(filepath).stem
+    invoice_nr, date = filename.split("-")
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}", ln=1)
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    column = df.columns
+    column = [item.replace("_", " ").title() for item in column]
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=column[0], border=1)
+    pdf.cell(w=70, h=8, txt=column[1], border=1)
+    pdf.cell(w=35, h=8, txt=column[2], border=1)
+    pdf.cell(w=30, h=8, txt=column[3], border=1)
+    pdf.cell(w=30, h=8, txt=column[4], border=1, ln=1)
+
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=35, h=8, txt=str(row["amount_purchased"]), align="C", border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
+    total_sum = str(df["total_price"].sum())
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=35, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=total_sum, border=1, ln=1)
+
+    # Total Sum
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=2)
+
+    # Company Image
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"Go Enjoy", align="C")
+    pdf.image("sloth.png", w=30)
+
+    pdf.output(f"PDFs/output{filename}.pdf")
--- app-pdf/main.py
+++ app-pdf/main.py
@@ -1,18 +1,41 @@
 from fpdf import FPDF
 import pandas as pd
 
-
 df = pd.read_csv("topics.csv")
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
-
+pdf.set_auto_page_break(auto=False, margin=0)
 
 for index, row in df.iterrows():
     pdf.add_page()
     pdf.set_font("Arial", style="B", size=25)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(200, 10, txt=f"{row['Topic']} : {row['Pages']}", ln=1, align="L")
-    pdf.line(10, 21, 200, 21)
 
+    # set the lines
+    a = 21
+    for i in range(20, 298, 10):
+        pdf.line(10, i, 200, i)
+        a = a + 10
+
+    # Set the footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+
+        # Set the footer
+        pdf.ln(265)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        # set the lines
+        for i in range(20, 298, 10):
+            pdf.line(10, i, 200, i)
+            a = a + 10
 
 pdf.output("op1.pdf")
